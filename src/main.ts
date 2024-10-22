d41e155bd6080a653b1153198dcaf49598a995f6
DJ Stalder (RIT Student) <djs8743@rit.edu>
2024-10-22 18:08:05 -0400

    Major restructuring

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,6 @@
 // deno-lint-ignore-file no-explicit-any no-window no-window-prefix
 import * as fn from "./functions.ts"
 
-/**
- * !TODO:
- * - ui/ux
- * - add album support
- * - add yt and apple music support (maybe)
- * - album art guessing game
- * - album comparison game
- * - score tracking
- * - switch over to people logging in w/ spotify // figure out api keys
- * - add daily rounds stored in a server for each artist (so there can be acloudyskyele, madeonle, etc.)
- * - save playlists even when browser closes
- * - add check to see if playlist is public
- * - when it trys again when song has no id, the play again doesn work
- * - excalmations marks are still included
- * - random symobls are stillin cluded
- * - consolidate guess inputs, make guesses visible when round ends
- * - guess dropdown (database)
- * - automatically play song/add play song button when round ends
- * - consolidate link input; Add checkbox toggles for mode
- */
-
 //#region VARIABLE DELCARATIONS
 
 // Global Variables
@@ -36,7 +15,6 @@ let startTime2: number;
 let startTime3: number;
 let songLength: number;
 let guesses: string[];
-let playlistOrArtist = false; // playlist = false, artist = true
 
 // Get HTML Elements
 const titleElement = document.getElementById('title') as HTMLElement;
@@ -44,7 +22,7 @@ const loadingElement = document.getElementById('loading') as HTMLElement;
 const correctOrNotElement = document.getElementById('correctOrNot') as HTMLElement;
 const processUrlButton = document.getElementById('processUrlButton') as HTMLButtonElement;
 const processArtistUrlButton = document.getElementById('processUrlButton1') as HTMLButtonElement;
-const playlistUrlInput = document.getElementById('playlistUrl') as HTMLInputElement;
+const urlInput = document.getElementById('urlInput') as HTMLInputElement;
 const artistUrlInput = document.getElementById('artistUrl') as HTMLInputElement;
 const inputLabel = document.getElementById('inputLabel') as HTMLLabelElement;
 const guessInput = document.getElementById('guessInput') as HTMLInputElement;
@@ -55,43 +33,13 @@ const imageUrlElement = document.getElementById('imageUrl') as HTMLImageElement;
 const audioElement = document.getElementById('audioElement') as HTMLAudioElement;
 const sourceElement = document.getElementById('previewUrl') as HTMLSourceElement;
 const newSongButton = document.getElementById('newSongButton') as HTMLButtonElement;
-const playlistDropdown = document.getElementById('playlistDropdown') as HTMLSelectElement;
-const removePlaylistButton = document.getElementById('removePlaylistButton') as HTMLButtonElement;
-const artistDropdown = document.getElementById('artistDropdown') as HTMLSelectElement;
+const optionDropdown = document.getElementById('optionDropdown') as HTMLSelectElement;
+const removeOptionButton = document.getElementById('removePlaylistButton') as HTMLButtonElement;
 const removeArtistButton = document.getElementById('removeArtistButton') as HTMLButtonElement;
-const artistToggleInput = document.getElementById('playlistOrArtist') as HTMLInputElement;
+const playlistCheckbox = document.getElementById('playlistCheckbox') as HTMLInputElement;
+const artistCheckbox = document.getElementById('artistCheckbox') as HTMLInputElement;
 const submitButton = document.getElementById('submitButton') as HTMLInputElement;
 const skipButton = document.getElementById('skipButton') as HTMLButtonElement;
-//#endregion
-
-//#region UTILITY FUNCTIONS
-
-// Function to load playlists from local storage on page load
-function loadPlaylistsFromLocalStorage() {
-    // Preserve the first option (assuming it's a placeholder like "Select a playlist")
-    const placeholderOption = playlistDropdown.options[0];
-
-    // Clear the dropdown except the placeholder
-    playlistDropdown.innerHTML = ''; // Clears all options
-    playlistDropdown.add(placeholderOption); // Re-add the placeholder option
-
-    const storedPlaylists = JSON.parse(localStorage.getItem('playlists') || '[]');
-    storedPlaylists.forEach((playlist: { id: string, name: string }) => {
-        addPlaylistToDropdown(playlist.id, playlist.name, false);
-    });
-
-    const artistPlaceholderOption = artistDropdown.options[0];
-
-    // Clear the dropdown except the placeholder
-    artistDropdown.innerHTML = ''; // Clears all options
-    artistDropdown.add(artistPlaceholderOption); // Re-add the placeholder option
-
-    const storedArtists = JSON.parse(localStorage.getItem('artists') || '[]');
-    storedArtists.forEach((artist: { id: string, name: string }) => {
-        addPlaylistToDropdown(artist.id, artist.name, true);
-    });
-}
-
 //#endregion
 
 //#region GAME LOGIC
@@ -99,9 +47,9 @@ function loadPlaylistsFromLocalStorage() {
 // Initialize the game by fetching a random song from the playlist
 async function initializeGame() {
     let randomSong;
-    if (playlistOrArtist) {
+    if (artistCheckbox.checked) {
         randomSong = await fn.getRandomSongFromArtist("https://open.spotify.com/artist/" + artistId)
-    } else {
+    } else if (playlistCheckbox.checked) {
         randomSong = await fn.getRandomSongFromPlaylist("https://open.spotify.com/playlist/" + playlistId)
     }
     
@@ -140,7 +88,7 @@ async function initializeGame() {
     });
 
     // If after 5 attempts no valid song is found, show an error
-    if ((!randomSong?.viable_source && !playlistOrArtist)) {
+    if ((!randomSong?.viable_source && playlistCheckbox.checked)) {
         loadingElement.style.visibility = 'visible';
         loadingElement.innerText = "Too much local songs! Please try a different playlist!";
         imageUrlElement.setAttribute("src", ""); // Clear any image
@@ -164,7 +112,7 @@ function playAndPauseAudio(duration: number, overrideStartTime?: number) {
 }
 
 // Function to add playlist to the dropdown and store it in local storage
-function addPlaylistToDropdown(playlistId: string, playlistName: string, artist: boolean) {
+function addPlaylistToDropdown(playlistId: string, playlistName: string) {
     // Ensure playlistName is valid (not undefined or empty)
     if (!playlistName) {
         console.error('Invalid playlist name');
@@ -172,8 +120,7 @@ function addPlaylistToDropdown(playlistId: string, playlistName: string, artist:
     }
 
     // Check if the playlist is already in the dropdown
-    let existingOption = Array.from(playlistDropdown.options).find(option => option.value === playlistId);
-    if (artist) existingOption = Array.from(artistDropdown.options).find(option => option.value === playlistId);
+    let existingOption = Array.from(optionDropdown.options).find(option => option.value === playlistId);
     if (existingOption) {
         return; // Don't add duplicates
     }
@@ -181,14 +128,10 @@ function addPlaylistToDropdown(playlistId: string, playlistName: string, artist:
     const option = document.createElement('option');
     option.value = playlistId;
     option.text = playlistName;
-    if (artist) {
-        artistDropdown.add(option);
-    } else {
-        playlistDropdown.add(option);
-    }
+    optionDropdown.add(option);
     
-    let dropdown = "playlists"
-    if (artist) dropdown = "artists"
+    let dropdown = "playlists";
+    if (artistCheckbox.checked) dropdown = "artists";
 
     // Store the playlist in local storage
     const storedPlaylists = JSON.parse(localStorage.getItem(dropdown) || '[]');
@@ -202,99 +145,93 @@ function addPlaylistToDropdown(playlistId: string, playlistName: string, artist:
 
 // Handle playlist URL input and add playlist to the dropdown without initializing the game
 processUrlButton.addEventListener('click', async () => {
-    const url = playlistUrlInput.value;
-    if (!url.includes("https://open.spotify.com/playlist/")) {
-        alert("Please enter a valid Spotify Playlist URL.");
-        return;
+    const url = urlInput.value;
+    if (playlistCheckbox.checked) {
+        if (!url.includes("https://open.spotify.com/playlist/")) {
+            alert("Please enter a valid Spotify Playlist URL.");
+            return;
+        }
+    
+        // Extract playlist ID from URL
+        playlistId = url.replace("https://open.spotify.com/playlist/", "").split("?")[0];
+    
+        // Fetch access token and playlist info
+        accessToken = await fn.getAccessToken();
+        const playlistData = await fn.fetchReference(accessToken, `playlists/${playlistId}`);
+    
+        // Check if playlistData has a valid name
+        if (playlistData.name) {
+            // Add playlist to the dropdown and local storage
+            addPlaylistToDropdown(playlistId, playlistData.name);
+    
+            // Clear the input field after adding the playlist
+            urlInput.value = '';
+        } else {
+            console.error('Invalid playlist data received');
+        }
+    } else if (artistCheckbox.checked) {
+        if (!url.includes("https://open.spotify.com/artist/")) {
+            alert("Please enter a valid Spotify Artist URL.");
+            return;
+        }
+    
+        artistId = url.replace("https://open.spotify.com/artist/", "").split("?")[0];
+    
+        accessToken = await fn.getAccessToken();
+        const artistData = await fn.fetchReference(accessToken, `artists/${artistId}`);
+    
+        if (artistData.name) {
+            addPlaylistToDropdown(artistId, artistData.name);
+            artistUrlInput.value = '';
+        } else {
+            console.error('Invalid artist data received');
+        }
     }
-
-    // Extract playlist ID from URL
-    playlistId = url.replace("https://open.spotify.com/playlist/", "").split("?")[0];
-
-    // Fetch access token and playlist info
-    accessToken = await fn.getAccessToken();
-    const playlistData = await fn.fetchReference(accessToken, `playlists/${playlistId}`);
-
-    // Check if playlistData has a valid name
-    if (playlistData.name) {
-        // Add playlist to the dropdown and local storage
-        addPlaylistToDropdown(playlistId, playlistData.name, false);
-
-        // Clear the input field after adding the playlist
-        playlistUrlInput.value = '';
-    } else {
-        console.error('Invalid playlist data received');
-    }
-});
-
-processArtistUrlButton.addEventListener('click', async () => {
-    const url = artistUrlInput.value;
-    if (!url.includes("https://open.spotify.com/artist/")) {
-        alert("Please enter a valid Spotify Artist URL.");
-        return;
-    }
-
-    artistId = url.replace("https://open.spotify.com/artist/", "").split("?")[0];
-
-    accessToken = await fn.getAccessToken();
-    const artistData = await fn.fetchReference(accessToken, `artists/${artistId}`);
-
-    if (artistData.name) {
-        addPlaylistToDropdown(artistId, artistData.name, true);
-        artistUrlInput.value = '';
-    } else {
-        console.error('Invalid artist data received');
-    }
+    
 });
 
 // Function to remove the selected playlist from the dropdown and local storage
-removePlaylistButton.addEventListener('click', () => {
-    const selectedPlaylistId = playlistDropdown.value;
-    if (!selectedPlaylistId) {
-        alert("Please select a playlist to remove.");
+removeOptionButton.addEventListener('click', () => {
+    const selectedOptionId = optionDropdown.value;
+    if (!selectedOptionId) {
+        alert("Please select an option to remove.");
         return;
     }
 
     // Remove from the dropdown
-    const selectedOption = playlistDropdown.querySelector(`option[value="${selectedPlaylistId}"]`);
+    const selectedOption = optionDropdown.querySelector(`option[value="${selectedOptionId}"]`);
     if (selectedOption) {
-        playlistDropdown.removeChild(selectedOption);
+        optionDropdown.removeChild(selectedOption);
+    }
+
+    // Check where item is being stored
+    let storageType = '';
+
+    if (playlistCheckbox.checked) {
+        storageType = 'playlists';
+    } else if (artistCheckbox.checked) {
+        storageType = 'artists';
     }
 
     // Remove from local storage
-    const storedPlaylists = JSON.parse(localStorage.getItem('playlists') || '[]');
-    const updatedPlaylists = storedPlaylists.filter((playlist: { id: string }) => playlist.id !== selectedPlaylistId);
-
+    const storedOptions = JSON.parse(localStorage.getItem(storageType) || '[]');
+    let updatedOptions;
+    if (playlistCheckbox.checked) {
+        updatedOptions = storedOptions.filter((playlist: { id: string }) => playlist.id !== selectedOptionId);
+    } else if (artistCheckbox.checked) {
+        updatedOptions = storedOptions.filter((artist: { id: string }) => artist.id !== selectedOptionId);
+    }
+    
     // Update local storage
-    localStorage.setItem('playlists', JSON.stringify(updatedPlaylists));
+    localStorage.setItem(storageType, JSON.stringify(updatedOptions));
 
     // Reset the dropdown to the placeholder option
-    playlistDropdown.value = ''; // This sets it to the first option, "Select a playlist"
-});
-
-removeArtistButton.addEventListener('click', () => {
-    const selectedArtistId = artistDropdown.value;
-    if (!selectedArtistId) {
-        alert("Please select a artist to remove.");
-        return;
-    }
-
-    const selectedOption = artistDropdown.querySelector(`option[value="${selectedArtistId}"]`);
-    if (selectedOption) {
-        artistDropdown.removeChild(selectedOption);
-    }
-
-    const storedArtists = JSON.parse(localStorage.getItem('artists') || '[]');
-    const updatedArtists = storedArtists.filter((artist: { id: string }) => artist.id !== selectedArtistId);
-
-    localStorage.setItem('artists', JSON.stringify(updatedArtists));
-
-    artistDropdown.value = ''; 
+    optionDropdown.value = ''; // This sets it to the first option
 });
 
 // Function to handle playlist selection change
-playlistDropdown.addEventListener('change', async () => {
-    const selectedPlaylistId = playlistDropdown.value;
+optionDropdown.addEventListener('change', async () => {
+    const selectedPlaylistId = optionDropdown.value;
     if (selectedPlaylistId) {
         playlistId = selectedPlaylistId;
 
@@ -303,23 +240,11 @@ playlistDropdown.addEventListener('change', async () => {
             accessToken = await fn.getAccessToken();
         }
 
-        titleElement.innerText = "Welcome to Playlistle! The song guessing game??????"
-
-        // Initialize the game with the selected playlist
-        await initializeGame();
-    }
-});
-
-artistDropdown.addEventListener('change', async () => {
-    const selectedArtistId = artistDropdown.value;
-    if (selectedArtistId) {
-        artistId = selectedArtistId;
-
-        // Fetch access token if not already available
-        if (!accessToken) {
-            accessToken = await fn.getAccessToken();
+        if (playlistCheckbox.checked) {
+            titleElement.innerText = "Welcome to Playlistle! The song guessing game??????"
+        } else if (artistCheckbox.checked) {
+            titleElement.innerText = "Welcome to " + optionDropdown.options[optionDropdown.selectedIndex].innerText + "le! The song guessing game??????"
         }
-        titleElement.innerText = "Welcome to " + artistDropdown.options[artistDropdown.selectedIndex].innerText + "le! The song guessing game??????"
 
         // Initialize the game with the selected playlist
         await initializeGame();
@@ -364,9 +289,24 @@ volumeSlider.addEventListener('input', () => {
     audioElement.volume = parseFloat(volumeSlider.value);
 });
 
+// Playlist Toggle
+playlistCheckbox.addEventListener('click', () => {
+    artistCheckbox.disabled = false;
+    artistCheckbox.checked = false;
+
+    playlistCheckbox.disabled = true;
+
+    urlInput.placeholder="https://open.spotify.com/playlist/...";
+})
+
 // Artist Toggle
-artistToggleInput.addEventListener('click', () => {
-    playlistOrArtist = !playlistOrArtist
+artistCheckbox.addEventListener('click', () => {
+    playlistCheckbox.disabled = false;
+    playlistCheckbox.checked = false;
+
+    artistCheckbox.disabled = true;
+
+    urlInput.placeholder="https://open.spotify.com/artist/...";
 })
 
 // Load playlists from local storage when the page is loaded
@@ -396,7 +336,6 @@ function guessHelper(input: string) {
         submitButton.disabled = true;
         skipButton.disabled = true;
         revealSongDetails();
-        return;
     }
     
     // Minor error (close guess)
@@ -458,6 +397,28 @@ function skipHandler() {
 function revealSongDetails() {
     songInfoElement.style.visibility = 'visible';
     imageUrlElement.style.visibility = 'visible';
+}
+
+// Function to load playlists from local storage on page load
+function loadPlaylistsFromLocalStorage() {
+    // Preserve the first option (assuming it's a placeholder like "Select a playlist")
+    const placeholderOption = optionDropdown.options[0];
+
+    // Clear the dropdown except the placeholder
+    optionDropdown.innerHTML = ''; // Clears all options
+    optionDropdown.add(placeholderOption); // Re-add the placeholder option
+
+    if (playlistCheckbox.checked) {
+        const storedPlaylists = JSON.parse(localStorage.getItem('playlists') || '[]');
+        storedPlaylists.forEach((playlist: { id: string, name: string }) => {
+            addPlaylistToDropdown(playlist.id, playlist.name);
+        });
+    } else if (artistCheckbox.checked) {
+        const storedArtists = JSON.parse(localStorage.getItem('artists') || '[]');
+        storedArtists.forEach((artist: { id: string, name: string }) => {
+            addPlaylistToDropdown(artist.id, artist.name);
+        });
+    }
 }
 
 //#endregion
