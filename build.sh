f214d6efdc53a7b20ab93c6363cce5a65f6037f1
nasafrasa <86539501+Nasafrasa@users.noreply.github.com>
2024-10-27 14:09:19 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-DENO_VERSION="v1.34.2"
+DENO_VERSION="v1.40.2"
 DENOFLARE_VERSION="v0.6.0"
-curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL=./deno-v1.40.2 sh -s v1.40.2
-NO_COLOR=1 DENO_VERSION=v1.40.2 DENOFLARE_VERSION=${DENOFLARE_VERSION} \
-    ./deno-v1.40.2/bin/deno run --allow-all \
-    https://raw.githubusercontent.com/skymethod/denoflare/v0.6.0/cli/cli.ts \
-    site generate . ./output --verbose
+curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL=./deno-$DENO_VERSION sh -s $DENO_VERSION
+NO_COLOR=1 DENO_VERSION=$DENO_VERSION DENOFLARE_VERSION=${DENOFLARE_VERSION} ./deno-$DENO_VERSION/bin/deno run --allow-all https://raw.githubusercontent.com/skymethod/denoflare/$DENOFLARE_VERSION/cli/cli.ts site generate . ./output --verbose
