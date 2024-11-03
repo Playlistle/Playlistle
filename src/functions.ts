57c50308716c4a48a9fe367317667e6c52dbbfeb
nasafrasa <86539501+Nasafrasa@users.noreply.github.com>
2024-11-03 00:51:50 -0700

    detective time

--- src/functions.ts
+++ src/functions.ts
@@ -190,6 +190,9 @@ export async function randomSongFromPlaylist(playlistUrl: string) {
         attempts++;
 
         const randomSongInfo = await fetchSpotify(`playlists/${playlistUrl}/tracks?limit=1&offset=${Math.floor(Math.random() * playlist.tracks.total)}`);
+        console.log(randomSongInfo)
+        console.log(playlist.tracks.total)
+        console.log(playlistUrl)
         if (!randomSongInfo.items[0].track) alert("nasa doesn't know what caused this error LMAO, just reload the page and u should be good")
         const track = await randomSongInfo.items[0].track
 
